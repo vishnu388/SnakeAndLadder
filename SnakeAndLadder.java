ff9814516c74247549f4d5e9ae48af7a3ccbfab9
vishnu <vishnu388@gmail.com>
2021-03-13 18:41:07 +0530

    [vishnu]: Added to play Two palyers

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,53 +1,93 @@
 public class SnakeAndLadder {
 
 public static void main(String args[]){
-	int pos=0;
-	int flag=0;
-	int pos_temp;
-	int dieRoll=0;
+ 			int positiona=0;
+		 	int positionb=0;
+			int aflag=0;
+			int bflag=0;
+			int aptmp;
+			int bptmp;
+			int dieRoll=0;
 
-	while(true) {
-		dieRoll++;
+			while(true) {
+				dieRoll++;
 
-		int die=(int)Math.floor(Math.random()*10)%6 + 1;
-		if(flag==1) {
-			int options=(int)Math.floor(Math.random()*10)%2 +1;
-			switch(options) {
-				case 1 : {
-					pos_temp=pos;
-					pos+=die;
-					if(pos>100){
-					pos=pos_temp;
-					System.out.println("\nPlayer need to get Die value "+(100-pos)+" to win\n");
+				int dieA=(int)Math.floor(Math.random()*10)%6 + 1;
+				if(aflag==1) {
+					int optionsA=(int)Math.floor(Math.random()*10)%2 +1;
+					switch(optionsA) {
+						case 1 : {
+							aptmp=positiona;
+							positiona+=dieA;
+							if(positiona>100){
+							positiona=aptmp;
+							System.out.println("\nPlayer-A need to get Die value "+(100-positiona)+" to win\n");
+								break;
+							}
+							System.out.println("\n Player-A  Rolled[DIE] | Die Value : "+dieA +"\n Player-A  Climbed Laddar with +"+dieA+"\n current position of a player-A:"+positiona+"\n");
+							break;
+						}
+						case 2:{
+							positiona-=dieA;
+							if(positiona<=0){
+								aflag=0;
+								positiona=0;
+								System.out.println("\n NO!! player-A is eliminated out of the game He needs one in order to enter the game\n");
+								break;
+								}
+							System.out.println("\n Player-A  Rolled[DIE] ! Die Value : "+dieA +"\n Player-A  bitten by snake with -"+dieA+"\n current position of a player-A:"+positiona+"\n");
+							break;
+					}
+				}
+				}
+
+			if(aflag==0 && dieA==1) {
+						aflag=1;
+						positiona=1;
+						System.out.println("\nPlayer-A  is entered into Game! \n  Player-A  position : "+positiona+"\n");
+						}
+			if(positiona>=100) {
+					System.out.println("\n !!Game Over!!\n results are displayed below.....\n\n Player-A  wins with "+dieRoll+ " Die Rolls\n");
+					return;
+					}
+			int dieB=(int)Math.floor(Math.random()*10)%6 + 1;
+			if(bflag==1) {
+				int optionsB=(int)Math.floor(Math.random()*10)%2 +1;
+				switch(optionsB) {
+					case 1 : {
+						bptmp=positionb;
+						positionb+=dieB;
+						if(positionb>100){
+						positionb=bptmp;
+						System.out.println("\nPlayer-B need to get Die value "+(100-positionb)+" to win\n");
+							break;
+						}
+						System.out.println("\n Player-B  Rolled[DIE] | Die Value : "+dieB +"\n Player-B  Climbed Laddar with +"+dieB+"\n current position of a player-B:"+positionb+"\n");
 						break;
 					}
-					System.out.println("\n Player  Rolled[DIE] | Die Value : "+die +"\n Player  Climbed Laddar with +"+die+"\n current position of a player:"+pos+"\n");
-					break;
-				}
-				case 2:{
-					pos-=die;
-					if(pos<=0){
-						flag=0;
-						pos=0;
-						System.out.println("\n NO!! player is eliminated out of the game He needs one in order to enter the game\n");
+					case 2:{
+						positionb-=dieB;
+						if(positionb<=0){
+							bflag=0;
+							positionb=0;
+							System.out.println("\n NO!! playerB is eliminated out of the game He needs one in order to enter the game\n");
+							break;
+							}
+						System.out.println("\n Player-B  Rolled[DIE] ! Die Value : "+dieB +"\n Player-B  bitten by snake with -"+dieB+"\n current position of a player-B:"+positionb+"\n");
 						break;
-						}
-					System.out.println("\n Player  Rolled[DIE] ! Die Value : "+die +"\n Player  bitten by snake with -"+die+"\n current position of a player:"+pos+"\n");
-					break;
-			}
-		}
-		}
-
-	if(flag==0 && die==1) {
-				flag=1;
-				pos=1;
-				System.out.println("\nPlayer  is entered into Game! \n  Player  position : "+pos+"\n");
 				}
-	if(pos>=100) {
-			System.out.println("\n !!Game Over!!\n results are displayed below.....\n\n Player  wins with "+dieRoll+ " Die Rolls\n");
-			return;
+			}
 			}
 
-	}
-}
-}
+		if(bflag==0 && dieB==1) {
+					bflag=1;
+					positionb=1;
+					System.out.println("\nPlayer-B  is entered into Game! \n  Player-B  position : "+positionb+"\n");
+					}
+		if(positionb>=100) {
+				System.out.println("\n !!Game Over!!\n results are displayed below.....\n\n Player-B wins with "+dieRoll+ " Die Rolls\n");
+				return;
+				}
+			}
+		}
+		}

11a2aa536ddb9f08c6711ffb32ff07e369b6221f
vishnu <vishnu388@gmail.com>
2021-03-13 18:34:49 +0530

    [vishnu]: Added to player position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,10 @@
 public class SnakeAndLadder {
 
 public static void main(String args[]){
-	 int pos=0;
-	 int flag=0;
-	 int pos_temp;
-	 int dieRoll=0;
+	int pos=0;
+	int flag=0;
+	int pos_temp;
+	int dieRoll=0;
 
 	while(true) {
 		dieRoll++;
@@ -16,12 +16,23 @@ public static void main(String args[]){
 				case 1 : {
 					pos_temp=pos;
 					pos+=die;
-					System.out.println("\n Player  Rolled[DIE] | Die Value : "+die +"\n Player  Climbed Laddar with +"+die+"\n");
+					if(pos>100){
+					pos=pos_temp;
+					System.out.println("\nPlayer need to get Die value "+(100-pos)+" to win\n");
+						break;
+					}
+					System.out.println("\n Player  Rolled[DIE] | Die Value : "+die +"\n Player  Climbed Laddar with +"+die+"\n current position of a player:"+pos+"\n");
 					break;
 				}
 				case 2:{
 					pos-=die;
-					System.out.println("\n Player  Rolled[DIE] ! Die Value : "+die +"\n Player  bitten by snake with -"+die+"\n");
+					if(pos<=0){
+						flag=0;
+						pos=0;
+						System.out.println("\n NO!! player is eliminated out of the game He needs one in order to enter the game\n");
+						break;
+						}
+					System.out.println("\n Player  Rolled[DIE] ! Die Value : "+die +"\n Player  bitten by snake with -"+die+"\n current position of a player:"+pos+"\n");
 					break;
 			}
 		}
@@ -33,7 +44,7 @@ public static void main(String args[]){
 				System.out.println("\nPlayer  is entered into Game! \n  Player  position : "+pos+"\n");
 				}
 	if(pos>=100) {
-			System.out.println("\n !!Game Over!!\n\n Player  wins with "+dieRoll+ " Die Rolls\n");
+			System.out.println("\n !!Game Over!!\n results are displayed below.....\n\n Player  wins with "+dieRoll+ " Die Rolls\n");
 			return;
 			}
 

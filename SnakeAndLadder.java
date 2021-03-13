6675ec416c27e1e633943bda523df04b2369d27a
vishnu <vishnu388@gmail.com>
2021-03-13 17:21:47 +0530

    [vishnu]: Added to The Player position is zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+public class SnakeAndLadder {
+
+
+	public static void main(String args[]) {
+		int intialposition=0;
+		int dieRoll=0;
+
+		System.out.println("the player position is intially zero");
+	}
+}

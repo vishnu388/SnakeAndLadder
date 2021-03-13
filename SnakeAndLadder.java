1abbc3fa8c019df556c50456d8f4f7d69bdbbf5b
vishnu <vishnu388@gmail.com>
2021-03-13 17:49:58 +0530

    [vishnu]: Added to Random numbers on die

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,10 +1,11 @@
 public class SnakeAndLadder {
 
-
 	public static void main(String args[]) {
-		int intialposition=0;
+		int position=0;
 		int dieRoll=0;
 
-		System.out.println("the player position is intially zero");
+		int die=(int) (Math.floor(Math.random()*10)%6+1);
+		System.out.println("The values on die is :" +die);
 	}
 }
+
